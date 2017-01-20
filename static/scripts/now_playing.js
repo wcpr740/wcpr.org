9ac2cb0e33e2c0013ff4132928c2dcf966e2f0a0
Dylan Praul <dylan.praul@gmail.com>
2017-01-20 04:31:19 -0500

    Get now_playing.js to work with progress bar, still some bugs to figure out.

--- static/scripts/now_playing.js
+++ static/scripts/now_playing.js
@@ -5,10 +5,10 @@ function loadNowPlaying() {
     $.ajax({
         'method': 'get',
         'url': NOW_PLAYING_URL,
-        'dataType': 'json',
-        'success': function(data) {
+        'dataType': 'jsonp',
+        'jsonpCallback': 'nowPlayingCallback',
+        'success': function() {
             delay_on_error = 1000;  // reset delay in case we had errors
-            nowPlayingCallback(data);
         },
         'error': function () {
             setTimeout(loadNowPlaying, delay_on_error);
@@ -18,7 +18,36 @@ function loadNowPlaying() {
 }
 
 function nowPlayingCallback(data) {
+    var text_container = document.getElementById('now_playing'),
+        progress_bar = document.getElementById('play_progress'),
+        now = data['now'];
 
+    // load current song into now playing
+    text_container.innerHTML = now['artist'] + ' - '  + now['title'];
+
+    // get progress bar ready to move
+    var start_pieces = now['start'].split(':'),
+        start_time = new Date();
+    start_time = start_time.setHours(start_pieces[0], start_pieces[1], start_pieces[2]);
+    if (start_time > Date.now())  {
+        // account for when day rolls over.
+        start_time.setDate(start_time.getDate() - 1);
+    }
+
+    var len_seconds = parseInt(now['len']),
+        elapsed_seconds = (Date.now() - start_time) / 1000,
+        percent_finished = elapsed_seconds / len_seconds * 100,
+        remaining_seconds = len_seconds - elapsed_seconds;  // get time in seconds
+
+    progress_bar.style.width = percent_finished + '%';
+    progress_bar.style.transition = 'width 0s linear';
+    setTimeout(function() {
+        progress_bar.style.transition = 'width ' + (remaining_seconds - 1) + 's linear';
+        progress_bar.style.width = '100%';
+    }, 1000);
+
+    // schedule next time to load what's playing
+    setTimeout(loadNowPlaying, (remaining_seconds + 1) * 1000);
 }
 
 
