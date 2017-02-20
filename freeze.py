12b50e64ee53af96b347bc6de5888a0d9a8b0e3b
Dylan Praul <dylan.praul@gmail.com>
2017-02-20 18:42:27 -0500

    Change freeze.py to not serve built files

--- freeze.py
+++ freeze.py
@@ -1,4 +1,7 @@
 from flask_site import freezer, flask_config
 
 if __name__ == '__main__':
-    freezer.run(debug=flask_config.get('DEBUG'))  # build and serve from build directory
+    if flask_config.get('DEBUG', True):
+        freezer.run(debug=True)  # build and serve from build directory
+    else:
+        freezer.freeze()  # only build, don't serve
